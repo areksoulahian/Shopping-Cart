e25853f51b571ab860ec9250bea6c2217bd22a5f
Arek Soulahian <arek.soulahian@gmail.com>
2022-01-15 13:54:10 -0800

    c

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ export default function Header(props) {
         </a>
       </div>
 
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <main>
           <nav>
             <a href="#/">Home</a>{" "}

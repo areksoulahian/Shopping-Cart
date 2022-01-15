f6e3d9564031bafcab4adabcc264d4e1601ce350
Arek Soulahian <arek.soulahian@gmail.com>
2022-01-15 13:05:12 -0800

    Updates

---
+++ precache-manifest.3f45eae9f6584af00957d13b38cb2346.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b893fc0850de5b9e55cb823960dda0",
+    "url": "/areksoulahian/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "0ce8c63468c51a0fa3ab",
+    "url": "/areksoulahian/Shopping-Cart/static/css/main.af5ae7c1.chunk.css"
+  },
+  {
+    "revision": "c8e786632728e7279bef",
+    "url": "/areksoulahian/Shopping-Cart/static/js/2.70a676c0.chunk.js"
+  },
+  {
+    "revision": "b59861b9ca6e7b8651484dd212de5308",
+    "url": "/areksoulahian/Shopping-Cart/static/js/2.70a676c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909d95284c50badf8e6e",
+    "url": "/areksoulahian/Shopping-Cart/static/js/3.04c3964a.chunk.js"
+  },
+  {
+    "revision": "0ce8c63468c51a0fa3ab",
+    "url": "/areksoulahian/Shopping-Cart/static/js/main.c293478e.chunk.js"
+  },
+  {
+    "revision": "a4e4311e1db7580544c5",
+    "url": "/areksoulahian/Shopping-Cart/static/js/runtime-main.5f619c9c.js"
+  }
+]);

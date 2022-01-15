a704f8fa656d0a16e45b6bbf4ac2603ede8b42a9
Arek Soulahian <arek.soulahian@gmail.com>
2022-01-15 13:15:19 -0800

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router } from "react-router-dom";
 
-export default function Header(props){
-    const { countCartItems } = props; 
-    return(
-        <header className="row block center">
-            <div>
-                <a href="#/" >
-                    <h1>Arek's Shopping Cart</h1>
-                </a>
-            </div>
+export default function Header(props) {
+  const { countCartItems } = props;
+  return (
+    <header className="row block center">
+      <div>
+        <a href="#/">
+          <h1>Arek's Shopping Cart</h1>
+        </a>
+      </div>
 
-            <Router>
-                <main>
-                    <nav>
-                        <a href="#/">Home</a> <a href="#/cart">
-                    Cart {''}
-                    {countCartItems ? (
-                        <button className="badge" >{countCartItems}</button>
-                    ): ( '')
-                }
-            </a> <a href="#/about">About</a> 
-                    </nav>
-                </main>
-            </Router>
+      <Router>
+        <main>
+          <nav>
+            <a href="#/">Home</a>{" "}
+            <a href="#/cart">
+              Cart {""}
+              {countCartItems ? (
+                <button className="badge">{countCartItems}</button>
+              ) : (
+                ""
+              )}
+            </a>{" "}
+            <a href="#/about">About</a>
+          </nav>
+        </main>
+      </Router>
 
-            <div>
-                <a href='#/signin'>Sign-in</a>
-            </div>
-        </header>
-    );
+      <div>
+        <a href="#/signin">Sign-in</a>
+      </div>
+    </header>
+  );
 }
